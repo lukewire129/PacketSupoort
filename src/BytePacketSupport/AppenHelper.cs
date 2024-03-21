071ff9a0eed86ca3c97915079b6337a53a2f66e0
lukewire129 <lukewire129@gmail.com>
2024-03-21 13:19:24 +0900

    first commit

---
+++ src/BytePacketSupport/AppenHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace BytePacketSupport
+{
+    public static class AppendHelper
+    {
+        public static byte[] AppendByte(this byte b, byte AppenByte)
+        {
+            byte[] ToTalBytes = new byte[] { b, AppenByte };
+
+            return ToTalBytes;
+        }
+
+        public static byte[] AppendByte(this byte[] bs, byte AppenByte)
+        {
+            byte[] ToTalBytes = new byte[bs.Length + 1];
+
+            Buffer.BlockCopy (bs, 0, ToTalBytes, 0, bs.Length);
+            ToTalBytes[bs.Length] = AppenByte;
+
+            return ToTalBytes;
+        }
+
+        public static byte[] AppendBytes(this byte b, byte[] AppenBytes)
+        {
+            byte[] ToTalBytes = new byte[1 + AppenBytes.Length];
+            ToTalBytes[0] = b;
+            Buffer.BlockCopy (AppenBytes, 0, ToTalBytes, 1, AppenBytes.Length);
+
+            return ToTalBytes;
+        }
+
+        public static byte[] AppendBytes(this byte[] bs, byte[] AppenBytes)
+        {
+            byte[] ToTalBytes = new byte[bs.Length + AppenBytes.Length];
+
+            Buffer.BlockCopy (bs, 0, ToTalBytes, 0, bs.Length);
+            Buffer.BlockCopy (AppenBytes, 0, ToTalBytes, bs.Length, AppenBytes.Length);
+
+            return ToTalBytes;
+        }
+
+        public static byte[] AppendBytes(this byte[] bs, byte[] AppenBytes, int offset, int count)
+        {
+            byte[] ToTalBytes = new byte[bs.Length + AppenBytes.Length];
+
+            Buffer.BlockCopy (bs, 0, ToTalBytes, 0, bs.Length);
+            Buffer.BlockCopy (AppenBytes, offset, ToTalBytes, bs.Length, count);
+
+            return ToTalBytes;
+        }
+    }
+}
---
+++ src/BytePacketSupport/DisplayHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace BytePacketSupport
+{
+    public static class DisplayHelper
+    {
+        public static string DisplayAscii(this byte[] b)
+        {
+            return Encoding.ASCII.GetString (b);
+        }
+        public static string Display(this byte b)
+        {
+            return b.ToString ("X2");
+        }
+
+        public static string Display(this byte[] b)
+        {
+            return string.Concat (Array.ConvertAll (b, byt => byt.ToString ("X2")));
+        }
+    }
+}
