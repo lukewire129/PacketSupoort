3e7b5184d8d3fea518ded54d3af0b3732e114b2e
lukewire129 <lukewire129@gmail.com>
2024-03-22 14:06:24 +0900

    byte Array <-> class 직렬화, 역직렬화 기능 추가

---
+++ src/BytePacketSupport/Attibutes/ByteSizeAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BytePacketSupport.Attibutes
+{
+
+    public class ByteSizeAttribute : Attribute
+    {
+        public int Size { get; }
+
+        public ByteSizeAttribute(int size)
+        {
+            Size = size;
+        }
+    }
+}
