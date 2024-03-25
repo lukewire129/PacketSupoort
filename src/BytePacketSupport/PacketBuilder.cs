7afb5252417573a8c182db3dfaeac98583356600
lukewire <lukewire129@gmail.com>
2024-03-25 12:04:20 +0900

    Add() => Append() 변경

--- src/BytePacketSupport/PacketBuilder.cs
+++ src/BytePacketSupport/PacketBuilder.cs
@@ -8,45 +8,45 @@ namespace BytePacketSupport
     {
         private List<byte> packetData = new List<byte> ();
 
-        public PacketBuilder Add(byte data)
+        public PacketBuilder Append(byte data)
         {
             packetData.Add (data);
 
             return this;
         }
 
-        public PacketBuilder Add(byte[] data)
+        public PacketBuilder Append(byte[] data)
         {
             packetData.AddRange(data);
 
             return this;
         }
 
-        public PacketBuilder Add(string ascii)
+        public PacketBuilder Append(string ascii)
         {
             packetData.AddRange (ByteConverter.GetByte (ascii));
             return this;
         }
 
-        public PacketBuilder Add(int intByte, Endian endian = Endian.BIG)
+        public PacketBuilder Append(int intByte, Endian endian = Endian.BIG)
         {
             packetData.AddRange (ByteConverter.GetByte (intByte, endian));
             return this;
         }
 
-        public PacketBuilder Add(long longByte, Endian endian = Endian.BIG)
+        public PacketBuilder Append(long longByte, Endian endian = Endian.BIG)
         {
             packetData.AddRange (ByteConverter.GetByte (longByte, endian));
             return this;
         }
 
-        public PacketBuilder Add(short shortByte, Endian endian = Endian.BIG)
+        public PacketBuilder Append(short shortByte, Endian endian = Endian.BIG)
         {
             packetData.AddRange (ByteConverter.GetByte (shortByte, endian));
             return this;
         }
 
-        public PacketBuilder Add<TSource>(TSource AppenClass) where TSource : class
+        public PacketBuilder Append<TSource>(TSource AppenClass) where TSource : class
         {
             packetData.AddRange (PacketParse.Serialization (AppenClass));
             return this;
