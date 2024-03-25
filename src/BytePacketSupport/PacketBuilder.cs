226ee184d90197e08577ae74abc275283f6604c1
lukewire <lukewire129@gmail.com>
2024-03-25 14:39:00 +0900

    Error 검출 메서드 추가 및 CRC, CheckSum 추가

--- src/BytePacketSupport/PacketBuilder.cs
+++ src/BytePacketSupport/PacketBuilder.cs
@@ -1,6 +1,9 @@
 ﻿using BytePacketSupport.Converter;
 using BytePacketSupport.Enums;
+using Mythosia.Integrity.Checksum;
+using Mythosia.Integrity.CRC;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BytePacketSupport
 {
@@ -59,9 +62,105 @@ namespace BytePacketSupport
             return this;
         }
 
+        public PacketBuilder ErrorDetection(Checksum8Type checkSum8)
+        {
+            byte[] errorcheck = new Checksum8 (checkSum8).Compute (this.packetData.ToArray ()).ToArray();
+            this.packetData.AddRange (errorcheck);
+
+            return this;
+        }
+
+        public PacketBuilder ErrorDetection(Checksum8Type checkSum8, int start)
+        {
+            byte[] errorcheck = new Checksum8 (checkSum8).Compute (this.packetData.Skip (start - 1).ToArray ()).ToArray ();
+            this.packetData.AddRange (errorcheck);
+
+            return this;
+        }
+
+        public PacketBuilder ErrorDetection(Checksum8Type checkSum8, int start, int count)
+        {
+            byte[] errorcheck = new Checksum8 (checkSum8).Compute (this.packetData.Skip (start - 1).Take (count).ToArray ()).ToArray ();
+            this.packetData.AddRange (errorcheck);
+
+            return this;
+        }
+
+        public PacketBuilder ErrorDetection(CRC8Type Crc8Type)
+        {
+            byte[] errorcheck = new CRC8 (Crc8Type).Compute (this.packetData.ToArray ()).ToArray ();
+            this.packetData.AddRange (errorcheck);
+
+            return this;
+        }
+
+        public PacketBuilder ErrorDetection(CRC8Type Crc8Type, int start)
+        {
+            byte[] errorcheck = new CRC8 (Crc8Type).Compute (this.packetData.Skip (start - 1).ToArray ()).ToArray ();
+            this.packetData.AddRange (errorcheck);
+            
+            return this;
+        }
+        public PacketBuilder ErrorDetection(CRC8Type Crc8Type, int start, int count)
+        {
+            byte[] errorcheck = new CRC8 (Crc8Type).Compute (this.packetData.Skip (start - 1).Take (count).ToArray ()).ToArray ();
+            this.packetData.AddRange (errorcheck);
+
+            return this;
+        }
+
+        public PacketBuilder ErrorDetection(CRC16Type Crc16Type)
+        {
+            byte[] errorcheck = new CRC16 (Crc16Type).Compute (this.packetData.ToArray ()).ToArray ();
+            this.packetData.AddRange (errorcheck);
+
+            return this;
+        }
+
+        public PacketBuilder ErrorDetection(CRC16Type Crc16Type, int start)
+        {
+            byte[] errorcheck = new CRC16 (Crc16Type).Compute (this.packetData.Skip (start - 1).ToArray ()).ToArray ();
+            this.packetData.AddRange (errorcheck);
+
+            return this;
+        }
+
+        public PacketBuilder ErrorDetection(CRC16Type Crc16Type, int start, int count)
+        {
+            byte[] errorcheck = new CRC16 (Crc16Type).Compute (this.packetData.Skip (start - 1).Take (count).ToArray ()).ToArray ();
+            this.packetData.AddRange (errorcheck);
+
+            return this;
+        }
+
+        public PacketBuilder ErrorDetection(CRC32Type Crc32Type)
+        {
+            byte[] errorcheck = new CRC32 (Crc32Type).Compute (this.packetData.ToArray ()).ToArray ();
+            this.packetData.AddRange (errorcheck);
+
+            return this;
+        }
+
+        public PacketBuilder ErrorDetection(CRC32Type Crc32Type, int start)
+        {
+            byte[] errorcheck = new CRC32 (Crc32Type).Compute (this.packetData.Skip (start - 1).ToArray ()).ToArray ();
+            this.packetData.AddRange (errorcheck);
+
+            return this;
+        }
+
+        public PacketBuilder ErrorDetection(CRC32Type Crc32Type, int start, int count)
+        {
+            byte[] errorcheck = new CRC32 (Crc32Type).Compute (this.packetData.Skip (start - 1).Take (count).ToArray ()).ToArray ();
+            this.packetData.AddRange (errorcheck);
+
+            return this;
+        }
+
         public byte[] Build()
         {
             return this.packetData.ToArray ();
         }
     }
 }
+
