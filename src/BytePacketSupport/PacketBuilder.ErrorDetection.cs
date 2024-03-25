18cbe492b8232c20210ed5da8531c581417fe4c0
lukewire129 <lukewire129@gmail.com>
2024-03-25 15:30:54 +0900

    Update .

--- src/BytePacketSupport/PacketBuilder.ErrorDetection.cs
+++ src/BytePacketSupport/PacketBuilder.ErrorDetection.cs
@@ -9,59 +9,6 @@ namespace BytePacketSupport
 {
     public partial class PacketBuilder
     {
-        private List<byte> packetData = new List<byte> ();
-
-        public PacketBuilder Append(byte data)
-        {
-            packetData.Add (data);
-
-            return this;
-        }
-
-        public PacketBuilder Append(byte[] data)
-        {
-            packetData.AddRange(data);
-
-            return this;
-        }
-
-        public PacketBuilder Append(List<byte> data)
-        {
-            packetData.AddRange (data);
-
-            return this;
-        }
-
-        public PacketBuilder Append(string ascii)
-        {
-            packetData.AddRange (ByteConverter.GetByte (ascii));
-            return this;
-        }
-
-        public PacketBuilder Append(int intByte, Endian endian = Endian.BIG)
-        {
-            packetData.AddRange (ByteConverter.GetByte (intByte, endian));
-            return this;
-        }
-
-        public PacketBuilder Append(long longByte, Endian endian = Endian.BIG)
-        {
-            packetData.AddRange (ByteConverter.GetByte (longByte, endian));
-            return this;
-        }
-
-        public PacketBuilder Append(short shortByte, Endian endian = Endian.BIG)
-        {
-            packetData.AddRange (ByteConverter.GetByte (shortByte, endian));
-            return this;
-        }
-
-        public PacketBuilder Append<TSource>(TSource AppenClass) where TSource : class
-        {
-            packetData.AddRange (PacketParse.Serialization (AppenClass));
-            return this;
-        }
-
         public PacketBuilder ErrorDetection(Checksum8Type checkSum8)
         {
             byte[] errorcheck = new Checksum8 (checkSum8).Compute (this.packetData.ToArray ()).ToArray();
@@ -155,11 +102,6 @@ namespace BytePacketSupport
             this.packetData.AddRange (errorcheck);
 
             return this;
-        }
-
-        public byte[] Build()
-        {
-            return this.packetData.ToArray ();
         }
     }
 }
---
+++ src/BytePacketSupport/PacketBuilder._.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace BytePacketSupport
+{
+    public partial class PacketBuilder
+    {
+        private List<byte> packetData = new List<byte> ();
+
+        public byte[] Build()
+        {
+            return this.packetData.ToArray ();
+        }
+    }
+}
+
