53f86b77f4b2e7f48d416c6f0c3661be1c93af18
lukewire <lukewire129@gmail.com>
2024-03-26 22:32:07 +0900

    - Checksum -> Compute 로 통합  - Readme 수정  - string GetBytes 누락으로 추가

--- src/BytePacketSupport/PacketBuilder.Section.cs
+++ src/BytePacketSupport/PacketBuilder.Section.cs
@@ -34,53 +34,6 @@ namespace BytePacketSupport
             return this;
         }
 
-        public PacketBuilder Checksum(string key, Checksum8Type checksum8Type)
-        {
-            if (this.packetData.Count () == 0)
-                return this;
-
-            if (byteKeyPoint.ContainsKey (key))
-                return this;
-
-            this.Compute (checksum8Type, byteKeyPoint[key].Item1, byteKeyPoint[key].Item2 - byteKeyPoint[key].Item1);
-            return this;
-        }
-
-        public PacketBuilder Checksum(string key, CRC8Type crc8Type)
-        {
-            if (this.packetData.Count () == 0)
-                return this;
-
-            if (byteKeyPoint.ContainsKey (key))
-                return this;
-
-            this.Compute (crc8Type, byteKeyPoint[key].Item1, byteKeyPoint[key].Item2 - byteKeyPoint[key].Item1);
-            return this;
-        }
-        public PacketBuilder Checksum(string key, CRC16Type crc16Type)
-        {
-            if (this.packetData.Count () == 0)
-                return this;
-
-            if (byteKeyPoint.ContainsKey (key))
-                return this;
-
-            this.Compute (crc16Type, byteKeyPoint[key].Item1, byteKeyPoint[key].Item2 - byteKeyPoint[key].Item1);
-            return this;
-        }
-
-        public PacketBuilder Checksum(string key, CRC32Type crc32Type)
-        {
-            if (this.packetData.Count () == 0)
-                return this;
-
-            if (byteKeyPoint.ContainsKey (key))
-                return this;
-
-            this.Compute (crc32Type, byteKeyPoint[key].Item1, byteKeyPoint[key].Item2 - byteKeyPoint[key].Item1);
-            return this;
-        }
-
         public byte[] GetSection(string key) 
         {
             if (this.packetData.Count () == 0)
