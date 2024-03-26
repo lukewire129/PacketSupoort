5a713dce004437b1389474e898183f1241bb51e5
lukewire129 <lukewire129@gmail.com>
2024-03-26 16:37:51 +0900

    SerializeDesrialize  - Attribute 적용  - unsigned Type 적용

---
+++ src/AppendTest/SerializeDeserializeUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using BytePacketSupport;
+using BytePacketSupport.Attibutes;
+using BytePacketSupport.Enums;
+
+namespace AppendTest
+{
+    public class SerializeDeserializeUnitTest
+    {
+        public class Test3Packet
+        {
+            [Endian (Endian.BIG)]
+            public int Value;
+            [Endian (Endian.LITTLE)]
+            public int Value1;
+            [ByteSize (3)]
+            public string Value3;
+        }
+
+
+        [Fact]
+        public void DeserializeEndianAttribute()
+        {
+            var test = new byte[] { 0x01, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x61, 0x62, 0x63 };
+            var aaa = PacketParse.Deserialize<Test3Packet> (test);
+        }
+        [Fact]
+        public void SerializeEndianAttribute()
+        {
+            var aaa = new Test3Packet ()
+            {
+                Value = 1,
+                Value1 = 1,
+                Value3 = "abc",
+            };
+
+            var abc = PacketParse.Serialize (aaa);
+        }
+    }
+}
---
+++ src/BytePacketSupport/Attibutes/EndianAttribute.cs
@@ -0,0 +1,16 @@
+﻿using BytePacketSupport.Enums;
+using System;
+
+namespace BytePacketSupport.Attibutes
+{
+    [AttributeUsage (AttributeTargets.Field, AllowMultiple = false)]
+    public class EndianAttribute : Attribute
+    {
+        public Endian _endian { get; set; } = Endian.LITTLE;
+
+        public EndianAttribute(Endian endian)
+        {
+            _endian = endian;
+        }
+    }
+}
