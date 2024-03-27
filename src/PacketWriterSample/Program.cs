93d5b5dc5222fd96953dcd5e15adc38de8293887
lukewire <lukewire129@gmail.com>
2024-03-27 13:46:36 +0900

    update .

--- src/PacketWriterSample/Program.cs
+++ src/PacketWriterSample/Program.cs
@@ -1,14 +1,35 @@
-﻿
-using BytePacketSupport;
+﻿using BytePacketSupport;
 
-byte[] bytes = new byte[]{ 0x01, 0x02, 0x03 };
+var builder1 = new PacketBuilder ()
+               .AppendInt32 (1)
+               .AppendUInt32 (2)
+               .AppendInt16 (3)
+               .AppendUInt16 (4)
+               .AppendInt64 (5)
+               .AppendUInt64 (6)
+               .Build ();
 
-var abc = PacketParse.DeserializeObject<abc> (bytes);
-Console.WriteLine ();
+Console.WriteLine(builder1.ToHexString ());
 
-class abc
-{
-    public byte a;
-    public byte b;
-    public byte c;
-}
+var builder2 = new PacketBuilder ()
+               .Appendint (1)
+               .Appenduint (2)
+               .Appendshort (3)
+               .Appendushort (4)
+               .Appendlong (5)
+               .Appendulong (6)
+               .Build ();
+
+Console.WriteLine (builder2.ToHexString ());
+
+
+var builder3 = new PacketBuilder ()
+               .@int (1)
+               .@uint (2)
+               .@short (3)
+               .@ushort (4)
+               .@long (5)
+               .@ulong (6)
+               .Build ();
+
+Console.WriteLine (builder3.ToHexString ());
