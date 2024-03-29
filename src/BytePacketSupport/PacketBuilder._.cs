a5902a85aa391567ee16e6338e2300f3b8c4e82f
lukewire <lukewire129@gmail.com>
2024-03-29 11:21:57 +0900

    Update .

--- src/BytePacketSupport/PacketBuilder._.cs
+++ src/BytePacketSupport/PacketBuilder._.cs
@@ -141,30 +141,6 @@ namespace BytePacketSupport
             this.AppendBytes (datas);
             return this;
         }
-        public PacketBuilder AppendByte(byte data)
-        {
-            return Append (data);
-        }
-
-        public PacketBuilder AppendBytes(IEnumerable<byte> datas)
-        {
-            return Append (datas);
-        }
-
-        public PacketBuilder AppendString(string ascii)
-        {
-            return Append (ascii);
-        }
-
-        public PacketBuilder AppendPacketBuilder(PacketBuilder builder)
-        {
-            return AppendBytes (builder.Build ());
-        }
-
-        public PacketBuilder AppendClass<TSource>(TSource AppendClass) where TSource : class
-        {
-            return Append (AppendClass);
-        }
 
         public byte[] Build()
         {
